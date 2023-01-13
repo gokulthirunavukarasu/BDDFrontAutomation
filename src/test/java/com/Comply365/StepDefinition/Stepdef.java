56f2515ee33543b25f56c38f5157d620a583a19d
unknown <gokul@promantus.com>
2023-01-14 02:27:04 +0530

    fix for scenerios

--- src/test/java/com/Comply365/StepDefinition/Stepdef.java
+++ src/test/java/com/Comply365/StepDefinition/Stepdef.java
@@ -29,8 +29,7 @@ public class Stepdef extends BaseClass  {
 			final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 			scenario.attach(screenshot, "image/png", "image");
 		}
-		driver.quit();
-		FileUtils.cleanDirectory(new File(downloadPath)); 
+		driver.quit(); 
 		System.out.println("EndTest");
 	}
 	
@@ -53,4 +52,42 @@ public class Stepdef extends BaseClass  {
 	public void user_login_must_be_successful() throws InterruptedException, IOException {
 		 CommonMethod.WaitUntilVisibility("SuccessfulLogin", 300);
 	}
+	
+	//////////////////////Create Customer/////////////////////////////////////////////
+	@Given("User navigate to the Dashboard page by logging in to nopCommerce")
+	public void user_navigate_to_the_dashboard_page_by_logging_in_to_nop_commerce() throws IOException {
+		CommonMethod.WaitUntilPresence("", 120);
+	    
+	}
+	@When("User clicks on Customers tab")
+	public void user_clicks_on_customers_tab() throws IOException, InterruptedException {
+		CommonMethod.RobustclickElementVisible("","");
+		CommonMethod.RobustclickElementVisible("","");
+	}
+	@And("User clicks on Add new button")
+	public void user_clicks_on_add_new_button() throws IOException, InterruptedException {
+		CommonMethod.RobustclickElementVisible("","");
+	    
+	}
+	@And("User enters data to the Customer info fields")
+	public void user_enters_data_to_the_customer_info_fields() {
+	    
+	    
+	}
+	@And("User clicks on save button")
+	public void user_clicks_on_save_button() throws IOException, InterruptedException {
+		CommonMethod.RobustclickElementVisible("","");
+	    
+	}
+	@Then("User will be redirected to Customers list page successfully")
+	public void user_will_be_redirected_to_customers_list_page_successfully() throws IOException {
+		CommonMethod.WaitUntilPresence("", 120);
+	    
+	}
+	@And("User verifies created customer in Customers list")
+	public void user_verifies_created_customer_in_customers_list() throws IOException {    
+		CommonMethod.WaitUntilPresence("", 120);
+	}
+	
+	/////////////////Logout///////////////////////////////////////////////
 }
